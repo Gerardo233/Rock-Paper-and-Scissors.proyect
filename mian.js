4330a0ba70e6358e059cd34f4d4b3c78c6130a10
GerardoMejia1107 <00104923@uca.edu.sv>
2023-09-29 20:48:39 -0600

    :sparkles: A function to create a random number was created, this will be interpreted as one of the weapons; from 1 to 3, also, the main function for the game logic was built

--- mian.js
+++ mian.js
@@ -23,3 +23,43 @@ let points_of_user = 0;
 
 //Reset Button
 const reset = document.getElementsByClassName('again')[0];
+
+//Random number
+function randomNumber() {
+	let computer = Math.trunc(Math.random() * 3 + 1);
+	return computer;
+}
+
+//Process of the game
+/*
+1 = rock
+2 = paper
+3 = scissors
+*/
+
+function Process(userChoice, computerChoice) {
+	if (userChoice === computerChoice) {
+		alert(`It's it a tie`);
+	} else if (
+		(userChoice == 1 && computerChoice == 3) ||
+		(userChoice == 2 && computerChoice == 1) ||
+		(userChoice == 3 && computerChoice == 2)
+	) {
+		alert(`Point for the user!`);
+		points_of_user += 1;
+		userPointsDisplay.innerHTML = points_of_user;
+	} else {
+		alert(`Point for the computer!`);
+		points_of_computer += 1;
+		computerPointsDisplay.innerHTML = points_of_computer;
+	}
+	/* console.log('User: ', points_of_user);
+	console.log('Computer: ', points_of_computer); */
+	if (points_of_user >= 3 && points_of_computer < points_of_user) {
+		text.innerHTML = userWinner;
+		DisableWeaponSelection();
+	} else if (points_of_computer >= 3 && points_of_computer > points_of_user) {
+		text.innerHTML = computerWinner;
+		DisableWeaponSelection();
+	}
+}
